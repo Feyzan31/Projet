3489d540c70f02fdb62447258716332a23a5b2c8
Feyzan31 <161055249+Feyzan31@users.noreply.github.com>
2024-12-11 00:44:51 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+// Définition de la classe Hello
+class Hello {
+    // Propriété pour stocker un nom
+    private $name;
+
+    // Constructeur pour initialiser la propriété
+    public function __construct($name = "World") {
+        $this->name = $name;
+    }
+
+    // Méthode pour afficher un message
+    public function sayHello() {
+        return "Hello, " . $this->name . "!";
+    }
+}
+
+// Exemple d'utilisation de la classe
+$hello = new Hello("Class");
+echo $hello->sayHello(); // Affichera : Hello, Class!
+?>
